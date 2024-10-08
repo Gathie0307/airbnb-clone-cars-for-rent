61deee22474aa83fee4a2595cfaa958bdb78cb20
Gayathri Krishnan <iamkgayu@gmail.com>
2024-10-08 12:11:17 +0100

    Added controller changes

---
+++ app/controllers/cars_controller.rb
@@ -0,0 +1,36 @@
+class CarsController < ApplicationController
+  def index
+    @cars = Car.all
+  end
+
+  def show
+    @car = Car.find(params[:id])
+  end
+
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = Car.new(car_params)
+    if @car.save
+      redirect_to car_path(@car)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @car = Car.find(params[:id])
+  end
+
+  def update
+    @car = Car.find(params[:id])
+    @car.update(car_params)
+    redirect_to car_path(@car)
+  end
+
+  def car_params
+    params.require(:car).permit(:brand, :model, :year)
+  end
+end
--- test/controllers/cars_controller_test.rb
+++ test/controllers/cars_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class BookingTest < ActiveSupport::TestCase
+class CarsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end

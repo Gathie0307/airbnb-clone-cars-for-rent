b6a068e9aad5c582acbfdd97eb726542cdb98a42
Gayathri Krishnan <iamkgayu@gmail.com>
2024-10-09 12:17:04 +0100

    Added basic UI changes

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :cars, except: %i[delete]
+  resources :cars, except: %i[destroy]
 end
